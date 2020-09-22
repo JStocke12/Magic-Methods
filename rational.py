6332e6c063d7ae37afb4f293b7d74bf55e54b6aa
JStocke12 <56052177+JStocke12@users.noreply.github.com>
2020-09-22 15:04:08 -0400

    Added protections to Rational __init__

--- rational.py
+++ rational.py
@@ -1,8 +1,18 @@
+from math import gcd
+
 class Rational:
 
     __init__(self, p, q):
-        self.p = p
-        self.q = q
+        if q == 0:
+            raise Exception("Cannot divide by 0")
+        if (not type(p) is int) or (not type(q) is int):
+            raise TypeError("p and q must both be integers")
+        if p == 0:
+            self.p = 0
+            self.q = 1
+        else:
+            self.p = p//gcd(p,q)
+            self.q = q//gcd(p,q)
 
     __float__(self):
         return self.p/self.q
